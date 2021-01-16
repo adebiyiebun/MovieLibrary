e419dd12db102e4f3c5644e0d529bbf29d5de7a6
adebiyiebun <wvbw15@durham.ac.uk>
2021-01-16 19:08:27 +0000

    added jquery

---
+++ public/index.js
@@ -0,0 +1,8 @@
+
+$.getJSON('https://api.themoviedb.org/3/search/movie?api_key=d43b2cf37b68ac54e47dbe77c0b7edb6&query=Jack+Reacher', function(data){
+    console.log(data);
+});
+
+
+
+// key for MOVIE API d43b2cf37b68ac54e47dbe77c0b7edb6
